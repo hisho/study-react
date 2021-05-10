510f7357b19ca95485fd1c1579298a96eddb9283
hisho <hisho.web@gmail.com>
2021-05-10 22:06:22 +0900

    ✨ add input

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, {ChangeEvent, useCallback, useState} from "react";
 
 export const HomePage: React.VFC = () => {
+  const [text, setText] = useState('');
+
+  const inputChange = useCallback((event: ChangeEvent<HTMLInputElement>) => {
+    setText(event.target.value);
+  }, [setText]);
+
   return (
     <div>
-      index page
+      <form>
+        <input type="text" value={text} onChange={inputChange} placeholder="入力してください"/>
+      </form>
     </div>
   )
 }
